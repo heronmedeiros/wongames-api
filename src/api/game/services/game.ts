0ec19ccd109cbe358068988587b754142ddac63f
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-20 14:57:36 -0300

    Sample using axios on Gog

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,7 +1,17 @@
 /**
  * game service
  */
-
+import axios from 'axios';
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::game.game');
+export default factories.createCoreService('api::game.game', () => ({
+  async populate(params) {
+    const gogUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game`;
+
+      const {
+        data: { products }
+      } = await axios.get(gogUrl);
+
+      console.log(products[0]);
+  }
+}));
