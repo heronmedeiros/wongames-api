0fb29be3a34409de4def83d53dd5452c353a0ce7
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-20 15:49:42 -0300

    Getting some description, shortDescription and rating

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -2,16 +2,44 @@
  * game service
  */
 import axios from 'axios';
+import { JSDOM } from 'jsdom';
+
 import { factories } from '@strapi/strapi';
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll('-','_').toLowerCase();
+
+  const body = await axios.get(`https://gog.com/game/${gogSlug}`);
+  const dom = new JSDOM(body.data);
+
+  const rawDescription = dom.window.document.querySelector(".description");
+  const description = rawDescription.innerHTML;
+  const shortDescription = rawDescription.textContent.slice(0, 160);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  )
+
+  return {
+    description,
+    shortDescription,
+    rating: ratingElement
+      ? ratingElement
+        .getAttribute('xlink:href')
+        .replace(/_/g, "")
+        .replace("#","")
+      : "BR0",
+  }
+}
 
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
-    const gogUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game`;
+    const gogUrl = `https://catalog.gog.com/v1/catalog?limit4&order=desc%3Atrending`;
 
       const {
         data: { products }
       } = await axios.get(gogUrl);
 
-      console.log(products[0]);
+      console.log(await getGameInfo(products[2].slug));
   }
 }));
