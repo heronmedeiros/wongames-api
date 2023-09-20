bb2d1b19c25d0cbcda8a3a136b60f6af863d1799
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-20 17:28:20 -0300

    Creating game

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,11 +1,10 @@
 /**
  * game service
  */
-import axios from 'axios';
-import slugify from 'slugify';
-import { JSDOM } from 'jsdom';
-
-import { factories } from '@strapi/strapi';
+import axios from "axios";
+import { JSDOM } from "jsdom";
+import slugify from "slugify";
+import { factories } from "@strapi/strapi";
 
 const gameService = "api::game.game";
 const publisherService = "api::publisher.publisher";
@@ -13,9 +12,36 @@ const developerService = "api::developer.developer";
 const categoryService = "api::category.category";
 const platformService = "api::platform.platform";
 
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll("-", "_").toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/game/${gogSlug}`);
+  const dom = new JSDOM(body.data);
+
+  const raw_description = dom.window.document.querySelector(".description");
+
+  const description = raw_description.innerHTML;
+  const short_description = raw_description.textContent.slice(0, 160);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  return {
+    description,
+    short_description,
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace("#", "")
+      : "BR0",
+  };
+}
+
 async function getByName(name, entityService) {
   const item = await strapi.service(entityService).find({
-    filters: { name }
+    filters: { name },
   });
 
   return item.results.length > 0 ? item.results[0] : null;
@@ -25,40 +51,12 @@ async function create(name, entityService) {
   const item = await getByName(name, entityService);
 
   if (!item) {
-    await strapi
-            .service(entityService)
-            .create({
-              data:{
-                name,
-                slug: slugify(name, { strict: true, lower: true }),
-              }
-        })
-  }
-}
-
-async function getGameInfo(slug) {
-  const gogSlug = slug.replaceAll('-','_').toLowerCase();
-
-  const body = await axios.get(`https://gog.com/game/${gogSlug}`);
-  const dom = new JSDOM(body.data);
-
-  const rawDescription = dom.window.document.querySelector(".description");
-  const description = rawDescription.innerHTML;
-  const shortDescription = rawDescription.textContent.slice(0, 160);
-
-  const ratingElement = dom.window.document.querySelector(
-    ".age-restrictions__icon use"
-  )
-
-  return {
-    description,
-    shortDescription,
-    rating: ratingElement
-      ? ratingElement
-        .getAttribute('xlink:href')
-        .replace(/_/g, "")
-        .replace("#","")
-      : "BR0",
+    await strapi.service(entityService).create({
+      data: {
+        name,
+        slug: slugify(name, { strict: true, lower: true }),
+      },
+    });
   }
 }
 
@@ -66,22 +64,17 @@ async function createManyToManyData(products) {
   const developersSet = new Set();
   const publishersSet = new Set();
   const categoriesSet = new Set();
-  const platformSet = new Set();
+  const platformsSet = new Set();
 
-  products.forEach(product => {
-    const {
-      developers,
-      publishers,
-      genres,
-      operatigSystems
-     } = product;
+  products.forEach((product) => {
+    const { developers, publishers, genres, operatingSystems } = product;
 
-    genres?.forEach(({ name}) => {
+    genres?.forEach(({ name }) => {
       categoriesSet.add(name);
     });
 
-    operatigSystems?.forEach((item) => {
-      platformSet.add(item);
+    operatingSystems?.forEach((item) => {
+      platformsSet.add(item);
     });
 
     developers?.forEach((item) => {
@@ -93,31 +86,72 @@ async function createManyToManyData(products) {
     });
   });
 
-  const createAll = (set, entityName) =>
+  const createCall = (set, entityName) =>
     Array.from(set).map((name) => create(name, entityName));
 
-
   return Promise.all([
-    ...createAll(developersSet, developerService),
-    ...createAll(publishersSet, publisherService),
-    ...createAll(categoriesSet, categoryService),
-    ...createAll(platformSet, platformService),
-  ])
+    ...createCall(developersSet, developerService),
+    ...createCall(publishersSet, publisherService),
+    ...createCall(categoriesSet, categoryService),
+    ...createCall(platformsSet, platformService),
+  ]);
 }
 
-export default factories.createCoreService('api::game.game', () => ({
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, gameService);
+
+      if (!item) {
+        console.info(`>>>Creating: ${product.title}...`);
+
+        const game = await strapi.service(`${gameService}`).create({
+          data: {
+            name: product.title,
+            slug: product.slug,
+            price: product.price.finalMoney.amount,
+            release_date: new Date(product.releaseDate),
+            categories: await Promise.all(
+              product.genres.map(({ name }) => getByName(name, categoryService))
+            ),
+            platforms: await Promise.all(
+              product.operatingSystems.map((name) =>
+                getByName(name, platformService)
+              )
+            ),
+            developers: await Promise.all(
+              product.developers.map((name) =>
+                getByName(name, developerService)
+              )
+            ),
+            publisher: await Promise.all(
+              product.publishers.map((name) =>
+                getByName(name, publisherService)
+              )
+            ),
+            ...(await getGameInfo(product.slug)),
+            publishedAt: new Date(),
+          },
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
+export default factories.createCoreService(gameService, () => ({
   async populate(params) {
-    const gogUrl = `https://catalog.gog.com/v1/catalog?limit4&order=desc%3Atrending`;
+    const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
 
     const {
-      data: { products }
-    } = await axios.get(gogUrl);
+      data: { products },
+    } = await axios.get(gogApiUrl);
 
-    await createManyToManyData(
-      [
-        products[12],
-        products[13],
-      ]
-    );
-  }
+    await createManyToManyData([products[0], products[2]]);
+
+    await createGames([products[0], products[2]]);
+
+    // console.log(await getGameInfo(products[2].slug));
+  },
 }));
